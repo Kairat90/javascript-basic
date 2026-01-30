481c048e31e97e8d7b35cc25bf27ea268654d987
Kairat <kairat777dk@gmail.com>
2026-01-30 22:45:54 +0500

    имя, возвраст

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+// const answer = prompt("Where are you from?").toLowerCase;
+// console.log(`You are from ${answer}.`);
+// if (answer.toLowerCase() === "earth")
+
+let user = prompt("Как тебя зовут?");
+let age = prompt("Сколько тебе лет?");
+let status = "online";
+console.log(`Здравствуй, ${user}. Тебе ${age} лет. Статус: ${status}.`);
